9f1ea9cac55fb8cd1c0b6d2a49d067ece67647b0
ersinfurkan <75623954+ersinfurkan@users.noreply.github.com>
2021-12-10 22:28:03 +0300

    Yapay Zeka
    Aralık Ayı Yapay Zeka Ödevi

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int ID;
+            String SEHIR;
+            float DOZ;
+            float TOPLAM;
+            
+
+            Console.WriteLine("ID giriniz: ");
+            ID = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Şehir giriniz: ");
+            SEHIR = Console.ReadLine();
+
+            Console.WriteLine("Toplam Doz sayısı: ");
+            DOZ = float.Parse(Console.ReadLine());
+
+            Console.WriteLine("Toplam: ");
+            TOPLAM = float.Parse(Console.ReadLine());
+
+ 
+
+
+            MLModel1.ModelInput sampleData = new MLModel1.ModelInput()
+            {
+                ID = ID,
+                SEHIR = SEHIR,
+                DOZ = DOZ,
+                TOPLAM = TOPLAM,
+            };
+
+            // Make a single prediction on the sample data and print results
+            var predictionResult = MLModel1.Predict(sampleData);
+
+            Console.WriteLine("Using model to make single prediction -- Comparing actual NUFUS with predicted NUFUS from sample data...\n\n");
+
+
+            Console.WriteLine($"ID: {ID}");
+            Console.WriteLine($"SEHIR: {SEHIR}");
+            Console.WriteLine($"DOZ: {DOZ}");
+            Console.WriteLine($"TOPLAM: {TOPLAM}");
+            
+
+
+            Console.WriteLine($"\n\nToplam NUFUS: {predictionResult.Score}\n\n");
+            Console.WriteLine("=============== End of process, hit any key to finish ===============");
+            Console.ReadKey();
+        }
+    }
+}
